9f19bc9bc896c3cf2403edc53f67c1c8c7e84b27
MilanJanko <miljankovic91@gmail.com>
2023-08-11 21:39:32 +0200

    Fifth commit - Added Navigation Menu and Contact Us Page

--- Home.py
+++ Home.py
@@ -4,12 +4,11 @@ from PIL import Image
 
 st.set_page_config(page_title='Portfolio', layout="wide")
 
-"""Defining a function to resize image for web app, in order to make UI better."""
 
-
-def resize_image(img):
-    image_path = "images/" + img
-    image_size = (450, 280)
+# Defining a function to resize image for web app, in order to make UI better.#
+def resize_image(imag):
+    image_path = "images/" + imag
+    image_size = (500, 350)
     resized = Image.open(image_path)
     resized = resized.resize(image_size, Image.ANTIALIAS)
     return resized
@@ -48,7 +47,7 @@ st.markdown("<p class='big-font'> Below you can find more information about my "
             " By the way, picture above shows my small mops called Aki."
             " He enjoys walking all the time!</p>", unsafe_allow_html=True)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([3, 1, 3])
 df = pd.read_csv("projects.csv", sep=";")
 
 with col3:
@@ -57,6 +56,7 @@ with col3:
         st.write(row['description'])
         img = resize_image(row['image'])
         st.image(img)
+        st.write(f"[Source code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
@@ -64,3 +64,4 @@ with col4:
         st.write(row['description'])
         img = resize_image(row['image'])
         st.image(img)
+        st.write(f"[Source code]({row['url']})")
